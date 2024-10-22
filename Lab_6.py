90d40ea9220cedfab8053b55e139f32fa456cdb0
xovers-ufl <xovers@ufl.edu>
2024-10-22 19:45:03 -0400

    Update Lab_6.py
    Added encode function

--- Lab_6.py
+++ Lab_6.py
@@ -7,9 +7,36 @@ def print_menu():
           '\n3. Quit')
 
 
-def encode():
-    """increases every digit by 3"""
-    pass
+def encode(string):
+    """take in an 8-digit password in string format containing only integers.
+    After passing the password into the encoder, the encoder stores the encoded
+    password to a new variable, with each digit being shifted up by 3 numbers."""
+
+    encoded_string = '' # declare global string variable
+
+    for i in range(len(string)):
+        if string[i] == '0':
+            encoded_string += '3'
+        if string[i] == '1':
+            encoded_string += '4'
+        if string[i] == '2':
+            encoded_string += '5'
+        if string[i] == '3':
+            encoded_string += '6'
+        if string[i] == '4':
+            encoded_string += '7'
+        if string[i] == '5':
+            encoded_string += '8'
+        if string[i] == '6':
+            encoded_string += '9'
+        if string[i] == '7':
+            encoded_string += '0'
+        if string[i] == '8':
+            encoded_string += '1'
+        if string[i] == '9':
+            encoded_string += '2'
+
+    return encoded_string
 
 
 def decode():
